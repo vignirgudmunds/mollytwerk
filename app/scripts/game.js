92664a6b418260e78f2b479e69d4ad5001a4906f
Vignir Gudmundsson <vignirgudmunds@gmail.com>
2013-08-28 01:26:40 +0000

    Added fixed platforms and refactored css code

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -1,55 +1,101 @@
 /*global define, $ */
 
-define(['player'], function(Player) {
-  /**
-   * Main game class.
-   * @param {Element} el DOM element containig the game.
-   * @constructor
-   */
-  var Game = function(el) {
-    this.el = el;
-    this.player = new Player(this.el.find('.player'));
+define(['player', 'platform'], function(Player, Platform) {
+    /**
+     * Main game class.
+     * @param {Element} el DOM element containig the game.
+     * @constructor
+     */
+    var Game = function(el) {
+        this.el = el;
+        this.player = new Player(this.el.find('.player'));
+        this.platformsEl = el.find('.platforms');
 
-    // Cache a bound onFrame since we need it each frame.
-    this.onFrame = this.onFrame.bind(this);
-  };
+        // Cache a bound onFrame since we need it each frame.
+        this.onFrame = this.onFrame.bind(this);
 
-  /**
-   * Runs every frame. Calculates a delta and allows each game entity to update itself.
-   */
-  Game.prototype.onFrame = function() {
-    var now = +new Date() / 1000,
-        delta = now - this.lastFrame;
-    this.lastFrame = now;
+        this.platforms = [];
+        this.createPlatforms();
+    };
 
-    this.player.onFrame(delta);
+    Game.prototype.createPlatforms = function() {
+        // Ground
+        this.addPlatform(new Platform({
+            x: 100,
+            y: 418,
+            width: 800,
+            height: 10
+        }));
 
-    // Request next frame.
-    requestAnimFrame(this.onFrame);
-  };
+        // Floating platforms
+        this.addPlatform(new Platform({
+            x: 300,
+            y: 258,
+            width: 100,
+            height: 10
+        }));
+        this.addPlatform(new Platform({
+            x: 500,
+            y: 288,
+            width: 100,
+            height: 10
+        }));
+        this.addPlatform(new Platform({
+            x: 400,
+            y: 158,
+            width: 100,
+            height: 10
+        }));
+        this.addPlatform(new Platform({
+            x: 750,
+            y: 188,
+            width: 100,
+            height: 10
+        }));
+    };
 
-  /**
-   * Starts the game.
-   */
-  Game.prototype.start = function() {
-    // Restart the onFrame loop
-    this.lastFrame = +new Date() / 1000;
-    requestAnimFrame(this.onFrame);
-  };
+    Game.prototype.addPlatform = function(platform) {
+        this.platforms.push(platform);
+        this.platformsEl.append(platform.el);
+    };
 
-  /**
-   * Cross browser RequestAnimationFrame
-   */
-  var requestAnimFrame = (function() {
-    return window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function(/* function */ callback) {
-          window.setTimeout(callback, 1000 / 60);
-        };
-  })();
 
-  return Game;
+    /**
+     * Runs every frame. Calculates a delta and allows each game entity to update itself.
+     */
+    Game.prototype.onFrame = function() {
+        var now = +new Date() / 1000,
+            delta = now - this.lastFrame;
+        this.lastFrame = now;
+
+        this.player.onFrame(delta);
+
+        // Request next frame.
+        requestAnimFrame(this.onFrame);
+    };
+
+    /**
+     * Starts the game.
+     */
+    Game.prototype.start = function() {
+        // Restart the onFrame loop
+        this.lastFrame = +new Date() / 1000;
+        requestAnimFrame(this.onFrame);
+    };
+
+    /**
+     * Cross browser RequestAnimationFrame
+     */
+    var requestAnimFrame = (function() {
+        return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function(/* function */ callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+
+    return Game;
 });
--- app/scripts/player.js
+++ app/scripts/player.js
@@ -3,10 +3,13 @@
 define(['controls'], function(controls) {
 
     var PLAYER_SPEED = 200;
+    var JUMP_VELOCITY = 1000;
+    var GRAVITY = 4000;
 
     var Player = function(el) {
         this.el = el;
-        this.pos = { x: 0, y: 0 };
+        this.jumping = false;
+        this.pos = { x: 200, y: 215 };
         this.vel = { x: 0, y: 0 };
     };
 
@@ -22,8 +25,24 @@ define(['controls'], function(controls) {
             this.vel.x = 0;
         }
 
+        // Jumping
+        if (controls.keys.space) {
+            this.vel.y = -JUMP_VELOCITY;
+            this.jumping = true;
+        }
+
+        // Gravity
+        this.vel.y += GRAVITY * delta;
+
         this.pos.x += delta * this.vel.x;
         this.pos.y += delta * this.vel.y;
+
+        // Collision with ground
+        if (this.pos.y > 215) {
+            this.pos.y = 215;
+            this.vel.y = 0;
+            this.jumping = false;
+        }
 
         // Update UI
         this.el.css('transform', 'translate3d(' + this.pos.x + 'px,' + this.pos.y + 'px,0)');
